cc0455510077260492fed5645f11eda612e66ce2
Fares Kherbane <MY_NAME@example.com>
2022-04-19 11:42:17 +0100

    update

--- src/test.js
+++ src/test.js
@@ -1,6 +1,19 @@
 const container = document.getElementById('container');
 
+// Promise.all([fetch('test.json').then((x) => x.text())]).then(([sampleResp]) => {
+// 	container.innerHTML = sampleResp;
+// });
 
-Promise.all([fetch('test.json').then((x) => x.text())]).then(([sampleResp]) => {
-	container.innerHTML = sampleResp;
-});
+// const navBar = document.getElementById('navBar');
+// const offsetY = 100;
+// navBar.style.display = 'none';
+// window.addEventListener('scroll', (e) => {
+// 	console.log(window.pageYOffset);
+// 	if (window.pageYOffset > offsetY) {
+// 		navBar.style.display = 'flex';
+// 		navBar.style.paddingTop = '34px';
+// 		navBar.style.paddingBottom = '34px';
+// 	} else if (window.pageYOffset == 0) {
+// 		navBar.style.display = 'none';
+// 	}
+// });
