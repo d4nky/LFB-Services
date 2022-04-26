aec0f735535d323137aacc8f306c6d2dc69635d4
Fares Kherbane <MY_NAME@example.com>
2022-04-26 13:06:23 +0100

    Added responsive for tablets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
 	],
 	theme: {
 		screens: {
-			sm: '475px',
+			'sm': '475px',
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
 		},
 		fontFamily: {
 			roboto: ['sans-serif', 'Roboto'],
