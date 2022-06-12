b18c1bd5db9643c795a8c10bbdc7924fa7fcda60
Fares Kherbane <kherbanefares@gmail.com>
2022-06-12 10:13:53 +0100

    Update

--- dist/main.js
+++ dist/main.js
@@ -1,33 +1,21 @@
-const container = document.getElementById('news-container');
+// const container = document.getElementById('news-container');
 
-Promise.all([fetch('test.json').then((x) => x.text())]).then(([sampleResp]) => {
-	container.innerHTML = sampleResp;
-});
-const navBar = document.getElementById('navBar');
-const content = navBar.childNodes;
-const offsetY = 50;
-// navBar.style.opacity = '0%';
-// window.addEventListener('scroll', (e) => {
-// 	console.log(window.pageYOffset);
-// 	if (window.pageYOffset > offsetY) {
-// 		navBar.style.display = 'flex';
-// 		navBar.style.paddingTop = '34px';
-// 		navBar.style.paddingBottom = '34px';
-// 	} else if (window.pageYOffset == 0) {
-// 		navBar.style.display = 'none';
-// 	}
-// // });
-// console.log(content)
-// window.addEventListener('scroll', (e) => {
-// 	if (window.pageYOffset > offsetY) {
-// 		content.style.display = 'block';
-// 		navBar.style.height = '130px';
-// 	} else if (window.pageYOffset < offsetY) {
-// 		navBar.style.height = '0px';
-// 		content.style.display = 'none'
-// 	}
+// Promise.all([fetch('test.json').then((x) => x.text())]).then(([sampleResp]) => {
+// 	container.innerHTML = sampleResp;
 // });
 
 const mobileNavButton = document.getElementById('mobile-nav-button');
+const mobileMenu = document.getElementById('mobile-menu');
 
-mobileNavButton.addEventListener('click', (e) => {});
+mobileMenu.style.marginTop = '-500px';
+let menuToggle = false;
+mobileNavButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	if (menuToggle == false) {
+		mobileMenu.style.marginTop = '0px';
+		menuToggle = true;
+	} else if (menuToggle == true) {
+		mobileMenu.style.marginTop = '-500px';
+		menuToggle = false;
+	}
+});
